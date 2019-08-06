acc3640dd9868e7a673e301828544fd988c27830
mobinhassani <mobinhassani9374@gmail.com>
2019-08-06 15:51:23 +0430

    add HasAffiliateAttribute

---
+++ AccessManagement.UI/Base/Attributes/HAsAffiliateAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccessManagement.UI.Base.Attributes
+{
+    public class HasAffiliateAttribute : Attribute
+    {
+        public string ActionName { get; set; }
+    }
+}
