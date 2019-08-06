81964777f30073e38f883f281db876aa00bb3e2d
mobinhassani <mobinhassani9374@gmail.com>
2019-08-06 15:44:46 +0430

    develop of Home/INdex

--- AccessManagement.UI/Controllers/HomeController.cs
+++ AccessManagement.UI/Controllers/HomeController.cs
@@ -1,16 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using AccessManagement.UI.DataLayer;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AccessManagement.UI.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var strUserId = HttpContext.User.Claims.FirstOrDefault(c=>c.Type==ClaimTypes.NameIdentifier)?.Value;
+
+            var userId = Convert.ToInt32(strUserId);
+
+            var userAccess = _context
+              .UserAccesses
+              .Where(c => c.UserId.Equals(userId))
+              .ToList();
+
+            var modules = new SubSystemService()
+                .GetAllWithPermision(Assembly.GetExecutingAssembly(), userAccess);
+
+            return View(modules);
         }
     }
 }
